58d4360c4380b0a70feefefbf70ad547ecbe6ff6
zakaria chergui <z.chergui@esi-sba.dz>
2022-01-13 18:30:10 +0100

    loginui

--- webminproj/routes/web.php
+++ webminproj/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\TestController;
+use Faker\Provider\ar_EG\Text;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login/{name}',[TestController::class,'login']);
+
+
